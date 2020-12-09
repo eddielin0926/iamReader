121aff4a40b9e4d6dfd1e67b139cb6bd71698913
LinCyunKai <58349500+Eddielin0926@users.noreply.github.com>
2020-12-09 17:15:46 +0800

    Create book class

---
+++ iamReader/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace iamReader
+{
+    class Book
+    {
+        public FileInfo File;
+        public string Title;
+        public string Path;
+        public string Author;
+        public string Content;
+        public int Article;
+    }
+}

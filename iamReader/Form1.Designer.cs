56969a5d1d40c6afd5305d47db7466e8c3975822
TzuYun2000 <barbiehzy@gmail.com>
2020-12-12 23:28:47 +0800

    dark mode, adjust font size, scrollbar, homepage

--- iamReader/Form1.Designer.cs
+++ iamReader/Form1.Designer.cs
@@ -28,13 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.WebsiteTextBox = new System.Windows.Forms.TextBox();
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.DarkModeButton = new System.Windows.Forms.Button();
+            this.HomeButton = new System.Windows.Forms.Button();
+            this.NovelTextBox = new System.Windows.Forms.TextBox();
+            this.ChapterLabel = new System.Windows.Forms.Label();
+            this.WordSizeTrackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.WordSizeTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // WebsiteTextBox
+            // 
+            this.WebsiteTextBox.Location = new System.Drawing.Point(305, 294);
+            this.WebsiteTextBox.Name = "WebsiteTextBox";
+            this.WebsiteTextBox.Size = new System.Drawing.Size(359, 25);
+            this.WebsiteTextBox.TabIndex = 0;
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.DownloadButton.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.DownloadButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DownloadButton.Location = new System.Drawing.Point(692, 286);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(77, 34);
+            this.DownloadButton.TabIndex = 1;
+            this.DownloadButton.Text = "下載";
+            this.DownloadButton.UseVisualStyleBackColor = false;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // DarkModeButton
+            // 
+            this.DarkModeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.DarkModeButton.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.DarkModeButton.ForeColor = System.Drawing.Color.White;
+            this.DarkModeButton.Location = new System.Drawing.Point(931, 79);
+            this.DarkModeButton.Name = "DarkModeButton";
+            this.DarkModeButton.Size = new System.Drawing.Size(164, 38);
+            this.DarkModeButton.TabIndex = 3;
+            this.DarkModeButton.Text = "深色模式";
+            this.DarkModeButton.UseVisualStyleBackColor = false;
+            this.DarkModeButton.Click += new System.EventHandler(this.DarkModeButton_Click);
+            // 
+            // HomeButton
+            // 
+            this.HomeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.HomeButton.Font = new System.Drawing.Font("微軟正黑體", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.HomeButton.ForeColor = System.Drawing.Color.White;
+            this.HomeButton.Location = new System.Drawing.Point(931, 587);
+            this.HomeButton.Name = "HomeButton";
+            this.HomeButton.Size = new System.Drawing.Size(164, 42);
+            this.HomeButton.TabIndex = 4;
+            this.HomeButton.Text = "回主頁";
+            this.HomeButton.UseVisualStyleBackColor = false;
+            this.HomeButton.Click += new System.EventHandler(this.HomeButton_Click);
+            // 
+            // NovelTextBox
+            // 
+            this.NovelTextBox.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.NovelTextBox.Location = new System.Drawing.Point(12, 79);
+            this.NovelTextBox.MaximumSize = new System.Drawing.Size(890, 500);
+            this.NovelTextBox.MinimumSize = new System.Drawing.Size(890, 500);
+            this.NovelTextBox.Name = "NovelTextBox";
+            this.NovelTextBox.Size = new System.Drawing.Size(890, 500);
+            this.NovelTextBox.TabIndex = 6;
+            // 
+            // ChapterLabel
+            // 
+            this.ChapterLabel.AutoSize = true;
+            this.ChapterLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ChapterLabel.Location = new System.Drawing.Point(12, 26);
+            this.ChapterLabel.MaximumSize = new System.Drawing.Size(200, 30);
+            this.ChapterLabel.MinimumSize = new System.Drawing.Size(200, 30);
+            this.ChapterLabel.Name = "ChapterLabel";
+            this.ChapterLabel.Size = new System.Drawing.Size(200, 30);
+            this.ChapterLabel.TabIndex = 7;
+            this.ChapterLabel.Text = "章節";
+            this.ChapterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // WordSizeTrackBar
+            // 
+            this.WordSizeTrackBar.Location = new System.Drawing.Point(931, 137);
+            this.WordSizeTrackBar.Maximum = 30;
+            this.WordSizeTrackBar.Minimum = 10;
+            this.WordSizeTrackBar.Name = "WordSizeTrackBar";
+            this.WordSizeTrackBar.Size = new System.Drawing.Size(164, 56);
+            this.WordSizeTrackBar.TabIndex = 8;
+            this.WordSizeTrackBar.TickFrequency = 2;
+            this.WordSizeTrackBar.Value = 10;
+            this.WordSizeTrackBar.Scroll += new System.EventHandler(this.WordSizeTrackBar_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1134, 853);
+            this.Controls.Add(this.WordSizeTrackBar);
+            this.Controls.Add(this.ChapterLabel);
+            this.Controls.Add(this.NovelTextBox);
+            this.Controls.Add(this.HomeButton);
+            this.Controls.Add(this.DarkModeButton);
+            this.Controls.Add(this.DownloadButton);
+            this.Controls.Add(this.WebsiteTextBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "iamReader";
+            ((System.ComponentModel.ISupportInitialize)(this.WordSizeTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox WebsiteTextBox;
+        private System.Windows.Forms.Button DownloadButton;
+        private System.Windows.Forms.Button DarkModeButton;
+        private System.Windows.Forms.Button HomeButton;
+        private System.Windows.Forms.TextBox NovelTextBox;
+        private System.Windows.Forms.Label ChapterLabel;
+        private System.Windows.Forms.TrackBar WordSizeTrackBar;
     }
 }
 
--- iamReader/Form1.cs
+++ iamReader/Form1.cs
@@ -8,13 +8,125 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace iamReader
 {
+        
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            Home();
+           
+        }
+
+        private void DownloadButton_Click(object sender, EventArgs e) {
+            Read();
+        }
+
+
+        // 預設為淺色模式
+        bool DarkMode = false;
+
+        // 深色模式
+        private void DarkModeButton_Click(object sender, EventArgs e) {
+            // 改為淺色模式
+            if (DarkMode) {
+                DarkMode = false;
+
+                BackColor = Color.FromArgb(255, 255, 255);
+
+                NovelTextBox.BackColor = this.BackColor;
+                NovelTextBox.ForeColor = Color.FromArgb(64, 64, 64);
+
+                ChapterLabel.BackColor = this.BackColor;
+                ChapterLabel.ForeColor = Color.FromArgb(64, 64, 64);
+
+                DarkModeButton.BackColor = Color.FromArgb(64, 64, 64);
+                DarkModeButton.ForeColor = Color.FromArgb(255, 255, 255);
+
+                HomeButton.BackColor = Color.FromArgb(64, 64, 64);
+                HomeButton.ForeColor = Color.FromArgb(255, 255, 255);
+
+                DarkModeButton.Text = "深色模式";
+            } 
+            // 改為深色模式
+            else {
+                DarkMode = true;
+
+                BackColor = Color.FromArgb(64, 64, 64);
+
+                NovelTextBox.BackColor = this.BackColor;
+                NovelTextBox.ForeColor = Color.FromArgb(255, 255, 255);
+
+                ChapterLabel.BackColor = this.BackColor;
+                ChapterLabel.ForeColor = Color.FromArgb(255, 255, 255);
+                
+                DarkModeButton.BackColor = Color.FromArgb(255, 255, 255);
+                DarkModeButton.ForeColor = Color.FromArgb(64, 64, 64);
+
+                HomeButton.BackColor = Color.FromArgb(255, 255, 255);
+                HomeButton.ForeColor = Color.FromArgb(64, 64, 64);
+
+                DarkModeButton.Text = "淺色模式";
+            }
+        }
+
+        // 主頁
+        private void Home() {
+            WebsiteTextBox.Visible = true;
+            DownloadButton.Visible = true;
+            NovelTextBox.Visible = false;
+            DarkModeButton.Visible = false;
+            HomeButton.Visible = false;
+            ChapterLabel.Visible = false;
+            WordSizeTrackBar.Visible = false;
+        }
+
+        // 閱讀畫面
+        private void Read() {
+            WebsiteTextBox.Visible = false;
+            DownloadButton.Visible = false;
+            NovelTextBox.Visible = true;
+            DarkModeButton.Visible = true;
+            HomeButton.Visible = true;
+            ChapterLabel.Visible = true;
+            WordSizeTrackBar.Visible = true;
+
+            NovelTextBox.ReadOnly = true;
+            NovelTextBox.Multiline = true;
+            NovelTextBox.ScrollBars = ScrollBars.Vertical;
+            NovelTextBox.SelectedText = NovelTextBox.Text + "\r\n";
+            NovelTextBox.ScrollToCaret();
+
+            //測試用隨便打的
+            NovelTextBox.Text = "朋友買了一件衣料，綠色的底子帶白色方格，" +
+                "當她拿給我們看時，一位對圍棋十分感與趣的同學說：「啊，好像棋" +
+                "盤似的。」「我看倒有點像稿紙。」我說。「真像一塊塊綠豆糕。」一位外號" +
+                "叫「大食客」的同學緊接著說。 我們不禁哄堂大笑，同樣的一件衣料，每個人卻有" +
+                "不同的感覺。那位朋友連忙把衣料用紙包好，她覺得衣料就是衣料，" +
+                "不是棋盤，也不是稿紙，更不是綠豆糕。 人人的欣賞觀點不盡相同，那是和" +
+                "個人的性格與生活環境有關。如果經常逛布店的話，便會發現很少有一匹布沒有" +
+                "人選購過；換句話說，任何質地或花色的衣料，都有人欣賞它。一位鞋店的老闆曾" +
+                "指著櫥窗裡一雙式樣毫不漂亮的鞋子說：「無論怎麼難看的樣子，還是有人喜歡，所以" +
+                "不怕賣不出去。」 就以「人」來說，又何嘗不是如此？也許我們看某人不順眼，但是在" +
+                "他的男友和女友心中，往往認為他如「天仙」或「白馬王子」般地完美無缺。 人總會去尋求" +
+                "自己喜歡的事物，每個人的看法或觀點不同，並沒有什麼關係，重要的是──人與人之間，應該" +
+                "有彼此容忍和尊重對方的看法與觀點的雅量。如果他能從這扇門望見日出的美景，你又何必要" +
+                "他走向那扇窗去聆聽鳥鳴呢？你聽你的鳥鳴，他看他的日出，彼此都會有等量的美的感受。人與" +
+                "人偶有摩擦，往往都是由於缺乏那分雅量的緣故；因此，為了減少摩擦，增進和諧，我們必須努" +
+                "力培養雅量。";
+        }
+
+        // 回主頁
+        private void HomeButton_Click(object sender, EventArgs e) {
+            Home();
+        }
+
+        // 調整字體大小
+        private void WordSizeTrackBar_Scroll(object sender, EventArgs e) {
+            NovelTextBox.Font = new Font("微軟正黑體", WordSizeTrackBar.Value + 10);
         }
     }
 }

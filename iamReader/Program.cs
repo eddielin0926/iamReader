6f42db2f8d42a3c9a08682fa853592d81a090857
LinCyunKai <58349500+Eddielin0926@users.noreply.github.com>
2021-01-02 22:24:08 +0800

    Create library folder

--- iamReader/Program.cs
+++ iamReader/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -8,12 +9,33 @@ namespace iamReader
 {
     static class Program
     {
+        public static String Library = @".\library";
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            try
+            {
+                // Determine whether the directory exists.
+                if (Directory.Exists(Library))
+                {
+                    Console.WriteLine("The directory library exists already: {0}", Path.GetFullPath(Library));
+                }
+                else
+                {
+                    // Try to create the directory.
+                    DirectoryInfo di = Directory.CreateDirectory(Library);
+                    Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(Library));
+                    Console.WriteLine("Path: {0}", Path.GetFullPath(Library));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The directory creation failed: {0}", e.ToString());
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
